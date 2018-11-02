50b2343ed8fbdcdad2e8dc513b6ffe939681d34a
hananalshammary <hanandarwish1993@gmail.com>
2018-11-02 08:34:01 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,25 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [0];
+cardsInPlay.push("queen");
+cardsInPlay.push("queen");
+cardsInPlay.push("king");
+cardsInPlay.push("king");
+
+console.log("User flipped queen");
+
+var cardTwo = [3];
+cardsInPlay.push("queen");
+cardsInPlay.push("queen");
+cardsInPlay.push("king");
+cardsInPlay.push("king");
+
+console.log("User flipped queen");
+
+if (cardsInPlay[0] == cardsInPlay ) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
