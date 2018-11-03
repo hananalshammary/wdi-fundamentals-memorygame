71e303d00b08e378862b539eae7756d6528ffdbc
hananalshammary <hanandarwish1993@gmail.com>
2018-11-03 17:42:54 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,44 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = [0];
-cardsInPlay.push("queen");
-cardsInPlay.push("queen");
-cardsInPlay.push("king");
-cardsInPlay.push("king");
 
-console.log("User flipped queen");
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
 
-var cardTwo = [3];
-cardsInPlay.push("queen");
-cardsInPlay.push("queen");
-cardsInPlay.push("king");
-cardsInPlay.push("king");
+}
 
-console.log("User flipped queen");
+function flipCard (cardId) {
 
-if (cardsInPlay[0] == cardsInPlay ) {
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen" + cardsOne);
+
+console.log("User flipped queen" + cards[0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+console.log("User flipped queen" + cards[2]);
+
+
+cards[cardId].push(cardsInPlay);
+flipCard(0);
+flipCard(2);
+checkForMatch ();
+
+}
+
+console.log("User flipped " + cards[0]);
+console.log("User flipped " + cards[2]);
+
+
+if (cardsInPlay[0] == cardsInPlay) {
 	alert("You found a match!");
 }
 else {
-	alert("Sorry, try again.");
+		alert("Sorry, try again.");
 }
 
