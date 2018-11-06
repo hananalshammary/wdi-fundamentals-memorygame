d1b07fe1453342bdbfd1d6b053ec89eac7fc627c
hananalshammary <hanandarwish1993@gmail.com>
2018-11-06 06:30:44 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,6 +26,8 @@ var cards = [
 
 var cardsInPlay = [];
 
+
+
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
@@ -36,24 +38,35 @@ var checkForMatch = function () {
 };
 
 
-var flipCard = function (cardId){
+var flipCard = function (){
+	var cardId= this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();	
+	}
 };
 
-checkForMatch();
+/*checkForMatch();
 flipCard([0]);
 flipCard([2]);
+*/
 
 
 
+var createBoard = function () {
+	
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/bank.png');
+    cardElement.setAttribute('data-id', i);
+    document.getElementById('game-board').appendChild(cardElement);
+    cardElement.addEventListener('click', flipCard);
 
+}
+};
 
-
-
-
-
-
-
+createBoard();
